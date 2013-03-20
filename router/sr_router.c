95610458d232f6a00ddbd00ac806b0962fbe2acb
Keylime503 <keon.vafai@ucla.edu>
2013-03-19 18:38:57 -0700

    Debugging ICMP

--- router/sr_router.c
+++ router/sr_router.c
@@ -240,8 +240,6 @@ void sr_handlepacket(struct sr_instance* sr, uint8_t * packet/* lent */, unsigne
 	/* IP packet*/
 	if (ethtype == ethertype_ip) 
 	{ 
-		// printf("Packet is IP Packet\n");
-
 		/* Check that the packet is long enough for an IP header */
 		minlength += sizeof(sr_ip_hdr_t);
 		if (len < minlength) 
@@ -366,8 +364,6 @@ void sr_handlepacket(struct sr_instance* sr, uint8_t * packet/* lent */, unsigne
 	/* ARP Packet */
 	else if (ethtype == ethertype_arp) 
 	{ 
-    	// printf("Packet is ARP Packet\n");
-
     	minlength += sizeof(sr_arp_hdr_t);
     	if (len < minlength)
       	{
